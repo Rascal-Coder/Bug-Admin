35f290a1f43a356a73752f646bac4734070cdcc7
rascal <ras-cal@gmail.com>
2025-09-04 17:33:33 +0800

    wip: login user

--- src/_mock/index.ts
+++ src/_mock/index.ts
@@ -1,7 +1,8 @@
 import { setupWorker } from "msw/browser";
 import { menuList } from "./handlers/_menu";
+import { signIn, userList } from "./handlers/_user";
 
-const handlers = [menuList];
+const handlers = [menuList, signIn, userList];
 const worker = setupWorker(...handlers);
 
 export { worker };
--- src/pages/sys/login/index.tsx
+++ src/pages/sys/login/index.tsx
@@ -1,10 +1,18 @@
-import { toast } from "sonner";
+import { useNavigate } from "react-router";
+import { GLOBAL_CONFIG } from "@/global-config";
+import { useSignIn } from "@/store/userStore";
 import { Button } from "@/ui/button";
 export default function Login() {
+	const signIn = useSignIn();
+	const navigatge = useNavigate();
+	const handleSignIn = async () => {
+		await signIn({ username: "admin", password: "demo1234" });
+		navigatge(GLOBAL_CONFIG.defaultRoute, { replace: true });
+	};
 	return (
 		<Button
 			onClick={() => {
-				toast.success("Login success");
+				handleSignIn();
 			}}
 		>
 			Login
