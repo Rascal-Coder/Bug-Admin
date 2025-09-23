f463d6af0f7a51501daccb732dc1a07c15c50d3b
rascal <ras-cal@gmail.com>
2025-09-23 16:39:06 +0800

    wip: 优化layouts

--- src/layouts/weight/nav-user.tsx
+++ src/layouts/weight/nav-user.tsx
@@ -4,6 +4,7 @@ import { GLOBAL_CONFIG } from "@/global-config";
 import { useRouter } from "@/routes/hooks";
 import { useUserActions } from "@/store/userStore";
 import { Avatar, AvatarFallback, AvatarImage } from "@/ui/avatar";
+import { Button } from "@/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -13,8 +14,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/ui/dropdown-menu";
-// import { SidebarMenuItem } from "@/ui/sidebar";
-import { cn } from "@/utils";
+import { useAdminLayout } from "..";
 
 type NavUserProps = {
 	user: {
@@ -24,52 +24,9 @@ type NavUserProps = {
 	};
 };
 
-function SidebarMenuItem({ className, ...props }: React.ComponentProps<"li">) {
-	return (
-		<li
-			data-slot="sidebar-menu-item"
-			data-sidebar="menu-item"
-			className={cn("group/menu-item relative flex items-center justify-center", className)}
-			{...props}
-		/>
-	);
-}
-
-function SidebarMenuButton({
-	isActive = false,
-	className,
-}: React.ComponentProps<"button"> & {
-	isActive?: boolean;
-}) {
-	const button = (
-		<button
-			data-slot="sidebar-menu-button"
-			data-sidebar="menu-button"
-			data-active={isActive}
-			className={cn(
-				" peer/menu-button cursor-pointer flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
-				"hover:bg-accent hover:text-sidebar-accent-foreground",
-				"h-8 text-sm",
-				className,
-			)}
-			type="button"
-		/>
-	);
-
-	return button;
-}
-function SidebarMenu({ className, ...props }: React.ComponentProps<"ul">) {
-	return (
-		<ul
-			data-slot="sidebar-menu"
-			data-sidebar="menu"
-			className={cn("flex w-full min-w-0 flex-col gap-1", className)}
-			{...props}
-		/>
-	);
-}
-
 export function NavUser({ user }: NavUserProps) {
+	console.log("user", user);
+
 	const { replace } = useRouter();
 	const { clearUserInfoAndToken } = useUserActions();
 	const logout = () => {
@@ -81,79 +38,87 @@ export function NavUser({ user }: NavUserProps) {
 			replace(GLOBAL_CONFIG.loginRoute);
 		}
 	};
+	const { isMobile, collapseSidebar } = useAdminLayout();
 	return (
-		<SidebarMenu>
-			<SidebarMenuItem>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<SidebarMenuButton className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-							<Avatar className="h-8 w-8 rounded-full">
-								<AvatarImage src={user.avatar} alt={user.name} />
-								<AvatarFallback className="rounded-lg">SN</AvatarFallback>
-							</Avatar>
-							<div className="grid flex-1 text-start text-sm leading-tight">
-								<span className="truncate font-semibold">{user.name}</span>
-								<span className="truncate text-xs">{user.email}</span>
-							</div>
-							<ChevronsUpDown className="ms-auto size-4" />
-						</SidebarMenuButton>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent
-						className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-						// side={isMobile ? "bottom" : "right"}
-						side="right"
-						align="end"
-						sideOffset={4}
-					>
-						<DropdownMenuLabel className="p-0 font-normal">
-							<div className="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
-								<Avatar className="h-8 w-8 rounded-full">
-									<AvatarImage src={user.avatar} alt={user.name} />
-									<AvatarFallback className="rounded-lg">SN</AvatarFallback>
-								</Avatar>
-								<div className="grid flex-1 text-start text-sm leading-tight">
-									<span className="truncate font-semibold">{user.name}</span>
-									<span className="truncate text-xs">{user.email}</span>
-								</div>
-							</div>
-						</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						<DropdownMenuGroup>
-							<DropdownMenuItem asChild>
-								<NavLink to="https://github.com/Rascal-Coder/Bug-Admin" target="_blank">
-									<Book className="mr-2 h-4 w-4" />
-									文档
-								</NavLink>
-							</DropdownMenuItem>
-							<DropdownMenuItem asChild>
-								<NavLink to="https://github.com/Rascal-Coder/Bug-Admin" target="_blank" className="flex items-center">
-									<Github className="mr-2 h-4 w-4" />
-									Github
-								</NavLink>
-							</DropdownMenuItem>
-							<DropdownMenuItem asChild>
-								<NavLink
-									to="https://github.com/Rascal-Coder/Bug-Admin/issues"
-									target="_blank"
-									className="flex items-center"
-								>
-									<HelpCircle className="mr-2 h-4 w-4" />
-									问题 & 帮助
-								</NavLink>
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem
-							onClick={() => {
-								logout();
-							}}
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					type="button"
+					data-state={!collapseSidebar ? "collapsed" : "expanded"}
+					variant="ghost"
+					className="group h-12! data-[state=expanded]:bg-sidebar-accent data-[state=expanded]:text-sidebar-accent-foreground"
+				>
+					<Avatar className="h-8 w-8 rounded-full">
+						<AvatarImage src={user.avatar} alt={user.name} />
+						<AvatarFallback className="rounded-lg">SN</AvatarFallback>
+					</Avatar>
+					<div className="grid flex-1 text-start text-sm leading-tight">
+						<span className="truncate font-semibold">{user.name}</span>
+						<span className="truncate text-xs" title={user.email}>
+							{user.email}
+						</span>
+					</div>
+					<ChevronsUpDown className="hidden ms-auto size-4 group-data-[state=expanded]:block!" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent
+				className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+				side={isMobile ? "bottom" : "right"}
+				align="end"
+				sideOffset={4}
+			>
+				<DropdownMenuLabel className="p-0 font-normal">
+					<div className="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+						<Avatar className="h-8 w-8 rounded-full">
+							<AvatarImage src={user.avatar} alt={user.name} />
+							<AvatarFallback className="rounded-lg">SN</AvatarFallback>
+						</Avatar>
+						<div className="grid flex-1 text-start text-sm leading-tight">
+							<span className="truncate font-semibold">{user.name}</span>
+							<span className="truncate text-xs">{user.email}</span>
+						</div>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem asChild>
+						<NavLink to="https://github.com/Rascal-Coder/Bug-Admin" target="_blank">
+							<Book className="mr-2 h-4 w-4" />
+							文档
+						</NavLink>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<NavLink to="https://github.com/Rascal-Coder/Bug-Admin" target="_blank" className="flex items-center">
+							<Github className="mr-2 h-4 w-4" />
+							Github
+						</NavLink>
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<NavLink
+							to="https://github.com/Rascal-Coder/Bug-Admin/issues"
+							target="_blank"
+							className="flex items-center"
 						>
-							<LogOut className="mr-2 h-4 w-4" />
-							退出登录
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</SidebarMenuItem>
-		</SidebarMenu>
+							<HelpCircle className="mr-2 h-4 w-4" />
+							问题 & 帮助
+						</NavLink>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem
+					onClick={() => {
+						logout();
+					}}
+				>
+					<LogOut className="mr-2 h-4 w-4" />
+					退出登录
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+		// <SidebarMenu>
+		// 	<SidebarMenuItem>
+
+		// 	</SidebarMenuItem>
+		// </SidebarMenu>
 	);
 }
